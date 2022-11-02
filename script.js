d6337bbc5c48795be4475e0f3de6c2d43af3b05a
Jack Henderson <24339931@dundee.ac.uk>
2022-11-02 22:18:25 +0000

    Pulled in the Javascript and added the Portraits

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-function FSwitch() {
-    document.getElementById("FranklinImg").style.display = "none";
-    document.getElementById("FranklinText").style.display = "block";
-}
-function FDefault() {
-    document.getElementById("FranklinImg").style.display = "block";
-    document.getElementById("FranklinText").style.display = "none";
-}
+// function FSwitch() {
+//     document.getElementById("FranklinImg").style.display = "none";
+//     document.getElementById("FranklinText").style.display = "block";
+// }
+// function FDefault() {
+//     document.getElementById("FranklinImg").style.display = "block";
+//     document.getElementById("FranklinText").style.display = "none";
+// }
 
-function TSwitch() {
-    document.getElementById("TrevorImg").style.display = "none";
-    document.getElementById("TrevorText").style.display = "block";
-}
-function TDefault() {
-    document.getElementById("TrevorImg").style.display = "block";
-    document.getElementById("TrevorText").style.display = "none";
-}
+// function TSwitch() {
+//     document.getElementById("TrevorImg").style.display = "none";
+//     document.getElementById("TrevorText").style.display = "block";
+// }
+// function TDefault() {
+//     document.getElementById("TrevorImg").style.display = "block";
+//     document.getElementById("TrevorText").style.display = "none";
+// }
 
-function MSwitch() {
-    document.getElementById("MichealImg").style.display = "none";
-    document.getElementById("MichealText").style.display = "block";
-}
-function MDefault() {
-    document.getElementById("MichealImg").style.display = "block";
-    document.getElementById("MichealText").style.display = "none";
-}
+// function MSwitch() {
+//     document.getElementById("MichealImg").style.display = "none";
+//     document.getElementById("MichealText").style.display = "block";
+// }
+// function MDefault() {
+//     document.getElementById("MichealImg").style.display = "block";
+//     document.getElementById("MichealText").style.display = "none";
+// }
