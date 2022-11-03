ebf3b82fdd347d23732aadf482b05118e2d11558
Jack Henderson <24339931@dundee.ac.uk>
2022-11-03 11:58:41 +0000

    Removed Unnecacery animation code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function TSwitch(){
-    document.getElementById("child2").style.display = "none";
-    document.getElementById("child3").style.display = "none";
-    document.getElementById("child1").style.display = "none";
-    document.getElementById("TrevorImgOpenTab").style.display = "block";
+    document.getElementById("child2").style.display = "none"
+    document.getElementById("child3").style.display = "none"
+    document.getElementById("child1").style.display = "none"
+    document.getElementById("TrevorImgOpenTab").style.display = "block"
     document.getElementById("TrevorTBox1").style.display = "block"
     document.getElementById("TrevorTBox2").style.display = "block"
     document.getElementById("TrevorTBox3").style.display = "block"
@@ -10,12 +10,11 @@ function TSwitch(){
     document.getElementById("child1").style.justifyContent = "space-evenly"
 }
 function TCloseTab(){
-    document.getElementById("child2").style.display = "block";
-    document.getElementById("child3").style.display = "block";
-    document.getElementById("child1").style.display = "block";
-    document.getElementById("TrevorImgOpenTab").style.display = "none";
+    document.getElementById("child2").style.display = "block"
+    document.getElementById("child3").style.display = "block"
+    document.getElementById("child1").style.display = "block"
+    document.getElementById("TrevorImgOpenTab").style.display = "none "
     document.getElementById("TrevorTBox1").style.display = "none"
     document.getElementById("TrevorTBox2").style.display = "none"
     document.getElementById("TrevorTBox3").style.display = "none"
-    alert("AHH")
 }
