baaaf20fabe1d84fba4f138313522b77076a011d
Jack Henderson <24339931@dundee.ac.uk>
2022-11-03 12:18:12 +0000

    Added the Animation and Text for Micheal

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function TCloseTab(){
     document.getElementById("TrevorTBox2").style.display = "none"
     document.getElementById("TrevorTBox3").style.display = "none"
 }
+
+function MSwitch(){
+    document.getElementById("child2").style.display = "none"
+    document.getElementById("child3").style.display = "none"
+    document.getElementById("child1").style.display = "none"
+    document.getElementById("MichaelImgOpenTab").style.display = "block"
+    document.getElementById("MichaelTBox1").style.display = "block"
+    document.getElementById("MichaelTBox2").style.display = "block"
+    document.getElementById("MichaelTBox3").style.display = "block"
+    document.getElementById("child2").style.marginTop = "0px"
+    document.getElementById("child1").style.justifyContent = "space-evenly"
+}
+function MCloseTab(){
+    document.getElementById("child2").style.display = "block"
+    document.getElementById("child3").style.display = "block"
+    document.getElementById("child1").style.display = "block"
+    document.getElementById("MichaelImgOpenTab").style.display = "none "
+    document.getElementById("MichaelTBox1").style.display = "none"
+    document.getElementById("MichaelTBox2").style.display = "none"
+    document.getElementById("MichaelTBox3").style.display = "none"
+}
