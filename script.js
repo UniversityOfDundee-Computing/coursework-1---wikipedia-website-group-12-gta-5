0a366608a55bb7e03fa89c58f320074e096d9b11
Jack Henderson <24339931@dundee.ac.uk>
2022-11-03 00:42:35 +0000

    added trevor text and a light onclick

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-// function FSwitch() {
-//     document.getElementById("FranklinImg").style.display = "none";
-//     document.getElementById("FranklinText").style.display = "block";
-// }
-// function FDefault() {
-//     document.getElementById("FranklinImg").style.display = "block";
-//     document.getElementById("FranklinText").style.display = "none";
-// }
-
-// function TSwitch() {
-//     document.getElementById("TrevorImg").style.display = "none";
-//     document.getElementById("TrevorText").style.display = "block";
-// }
-// function TDefault() {
-//     document.getElementById("TrevorImg").style.display = "block";
-//     document.getElementById("TrevorText").style.display = "none";
-// }
-
-// function MSwitch() {
-//     document.getElementById("MichealImg").style.display = "none";
-//     document.getElementById("MichealText").style.display = "block";
-// }
-// function MDefault() {
-//     document.getElementById("MichealImg").style.display = "block";
-//     document.getElementById("MichealText").style.display = "none";
-// }
+function TSwitch(){
+    document.getElementById("child1").style.float = "right";
+    document.getElementById("child2").style.display = "none";
+    document.getElementById("child3").style.display = "none";
+    document.getElementById("fatherD").style.justifyContent ="flex-start"
+    document.getElementById("child1").style.marginLeft = "10%"
+    document.getElementById("TrevorText").style.display = "block"
+    document.getElementById("child1").style.marginTop = "0px"
+}
