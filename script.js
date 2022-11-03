f795bc43f90d73e3532ccbcf684a52be55625d09
Jack Henderson <24339931@dundee.ac.uk>
2022-11-03 12:25:16 +0000

    Added Franklin Functionality and animations

--- script.js
+++ script.js
@@ -39,3 +39,23 @@ function MCloseTab(){
     document.getElementById("MichaelTBox2").style.display = "none"
     document.getElementById("MichaelTBox3").style.display = "none"
 }
+function FSwitch(){
+    document.getElementById("child2").style.display = "none"
+    document.getElementById("child3").style.display = "none"
+    document.getElementById("child1").style.display = "none"
+    document.getElementById("FranklinImgOpenTab").style.display = "block"
+    document.getElementById("FranklinTBox1").style.display = "block"
+    document.getElementById("FranklinTBox2").style.display = "block"
+    document.getElementById("FranklinTBox3").style.display = "block"
+    document.getElementById("child2").style.marginTop = "0px"
+    document.getElementById("child1").style.justifyContent = "space-evenly"
+}
+function FCloseTab(){
+    document.getElementById("child2").style.display = "block"
+    document.getElementById("child3").style.display = "block"
+    document.getElementById("child1").style.display = "block"
+    document.getElementById("FranklinImgOpenTab").style.display = "none "
+    document.getElementById("FranklinTBox1").style.display = "none"
+    document.getElementById("FranklinTBox2").style.display = "none"
+    document.getElementById("FranklinTBox3").style.display = "none"
+}
