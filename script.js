a0546dada2301fdb7b2244aca4a9c7559e8c0b68
Jack Henderson <24339931@dundee.ac.uk>
2022-11-03 11:55:48 +0000

    Added a way to go back to closed tab

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function TSwitch(){
     document.getElementById("child1").style.marginTop = "0px"
     document.getElementById("child1").style.justifyContent = "space-evenly"
 }
+function TCloseTab(){
+    document.getElementById("child2").style.display = "block";
+    document.getElementById("child3").style.display = "block";
+    document.getElementById("child1").style.display = "block";
+    document.getElementById("TrevorImgOpenTab").style.display = "none";
+    document.getElementById("TrevorTBox1").style.display = "none"
+    document.getElementById("TrevorTBox2").style.display = "none"
+    document.getElementById("TrevorTBox3").style.display = "none"
+    alert("AHH")
+}
