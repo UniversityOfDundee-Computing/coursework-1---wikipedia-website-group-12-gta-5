20b1c3ecc8e3aa1de9d3182245467f568e6c867d
Jack Henderson <24339931@dundee.ac.uk>
2022-11-03 11:44:36 +0000

    added new animations and changed the way it works

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function TSwitch(){
     document.getElementById("child2").style.display = "none";
     document.getElementById("child3").style.display = "none";
-    document.getElementById("child1").style.marginLeft = "10%"
+    document.getElementById("child1").style.display = "none";
+    document.getElementById("TrevorImgOpenTab").style.display = "block";
     document.getElementById("TrevorTBox1").style.display = "block"
     document.getElementById("TrevorTBox2").style.display = "block"
     document.getElementById("TrevorTBox3").style.display = "block"
