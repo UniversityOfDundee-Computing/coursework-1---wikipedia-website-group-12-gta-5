0625212dc76fdaf9797002eb23a4664396dc93ad
Jack Henderson <24339931@dundee.ac.uk>
2022-11-03 11:11:41 +0000

    got the trevor text boxs working

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function TSwitch(){
-    document.getElementById("child1").style.float = "right";
     document.getElementById("child2").style.display = "none";
     document.getElementById("child3").style.display = "none";
-    document.getElementById("fatherD").style.justifyContent ="flex-start"
     document.getElementById("child1").style.marginLeft = "10%"
-    document.getElementById("TrevorText").style.display = "block"
+    document.getElementById("TrevorTBox1").style.display = "block"
+    document.getElementById("TrevorTBox2").style.display = "block"
+    document.getElementById("TrevorTBox3").style.display = "block"
     document.getElementById("child1").style.marginTop = "0px"
+    document.getElementById("child1").style.justifyContent = "space-evenly"
 }
